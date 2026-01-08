d9a616c23fa2c9bc8b00c948c3c2e0478a742008
Tsohnle95 <152450451+Tsohnle95@users.noreply.github.com>
2026-01-08 02:09:57 -0700

    start building chatbot header, implement js toggle button functionality, delete unused files

--- js/main.js
+++ js/main.js
@@ -1,41 +1,61 @@
 console.log('Hello World!');
 
 //remove landing page content and bring in chatbot content
-let pageOne = document.querySelectorAll('.page-1');
-let chatButtons = document.querySelectorAll('.chat-now');
+let pageOne = document.querySelector('.landing-page-content'); 
+let chatButtons = document.querySelectorAll('.chat-now'); 
 
 chatButtons.forEach(button => {
     button.addEventListener('click', () => {
         // hide landing-page elements
-        pageOne.forEach(element => {
-            element.style.display = 'none';
-        });
+       pageOne.style.display = 'none';
         //reveal chatbot elements
-        let chatBotContent = document.querySelectorAll('.chatbot-content');
-        chatBotContent.forEach(element => {
-            element.style.display = 'block';
-        });
-
+        let chatBotContent = document.querySelector('.chatbot-content');
+        chatBotContent.style.display = 'flex';
     });
 });
+
+
 
 
 // return landing page content and remove chatbot content
-let returnButtons = document.querySelectorAll('.chatbot-content');
+//disabled this as it prevents hamburger menu from showing
 
-returnButtons.forEach(element => {
-    element.addEventListener('click', () => {
-          //hide chatbot content
-        element.style.display = 'none';
-        //reveal landingpage content
-        pageOne.forEach(element => {
-            element.style.display = 'block';
-        });
-    });
+// let returnButtons = document.querySelectorAll('.chatbot-content');
+
+// returnButtons.forEach(element => {
+//     element.addEventListener('click', () => {
+//           //hide chatbot content
+//         element.style.display = 'none';
+//         //reveal landingpage content
+//         pageOne.style.display = 'block';
+//     });
+// });
+
+
+
+
+
+/**************** TOGGLE BUTTONS  **************** /
+/* hamburger-menu */
+btn = document.querySelector('.toggle-btn');
+nav = document.querySelector('nav');
+btn.addEventListener('click', () => {
+    nav.classList.toggle('show-nav');
 });
 
-// working on removing all the chatbot content, strange loop 
+/* button to close hamburger menu */
+let closeBtn = document.querySelector('.close-btn');
+closeBtn.addEventListener('click', () => {
+    nav.classList.remove('show-nav');
+})
 
-
+/* closes hamburger menu if user clicks outside of it*/
+window.addEventListener('click', (event) => {
+    if (nav.classList.contains('show-nav') && 
+        !nav.contains(event.target) && 
+        !btn.contains(event.target)) {
+        nav.classList.remove('show-nav');
+    }
+});
 
 
