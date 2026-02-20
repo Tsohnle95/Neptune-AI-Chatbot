7ef31c06a67cbd4dda81c238894eb74817b5d893
Ty <t.sohnle@shaw.ca>
2026-02-20 03:28:30 -0700

    adjust rate limiter

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -36,7 +36,7 @@ app.get('/api/health', (req, res) => {
 const limiter = rateLimit({
     //units: minutes/seconds/miliseconds, the memory buffer will reset every 15 minutes
   windowMs: 15 * 60 * 1000, 
-  max: 100, 
+  max: 15, 
   validate: { xForwardedForHeader: false }, 
   message: { error: 'Too many requests, please try again later.' }
 });
